05e20371400722ca597469b2004c36e58342c14e
KingJulianDev <kingurianjulian@gmail.com>
2020-07-27 20:16:41 +0300

    first

---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,18 @@
+import React from 'react' ; 
+import './spinner.css';
+
+const Spinner = () => {
+    return (
+        <div className="loadingio-spinner-double-ring-7g22j9zn4ns">
+            <div className="ldio-u1g8mfplmcf">
+                <div></div>
+                <div></div>
+<div><div></div></div>
+<div><div></div></div>
+<style type="text/css"></style>
+</div></div>
+//<style type="text/css"></style>
+    );
+};
+
+export default Spinner ;
